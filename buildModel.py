883fc03a742c753ab087d918438408f5b6326ac4
Riley <avatr.rm@gmail.com>
2018-04-10 21:34:47 -0400

    implemented undersampling and low frequency removal, eliminated iterations in favour of k-fold implementation later on.

--- buildModel.py
+++ buildModel.py
@@ -11,6 +11,7 @@ import operator
 
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
@@ -22,42 +23,50 @@ from sklearn.feature_selection import mutual_info_classif
 
 if __name__ == '__main__':
     #read data
-    data = pd.read_csv("videoCommentStats.csv")
+    path = os.getcwd() + '\\video_csvs\\'
+    data = pd.read_csv(path + "videoCommentStats.csv")
     categories = pd.read_csv("USvideos.csv")[ ['video_id','category_id']]
     #remove duplicates from categories
     categories.drop_duplicates(subset=None,keep='first',inplace=True)
     #join target to features
     data = pd.merge(data,categories,on='video_id',how='inner')
+    #remove infrequent categories
+    data = data[ (data['category_id'] != 43) & (data['category_id'] != 1) & (data['category_id'] != 2) & (data['category_id'] != 15) & (data['category_id'] != 19) & (data['category_id'] != 20) & (data['category_id'] != 27) & (data['category_id'] != 29) ]
+    #undersample category 24
+    sampleSize = sum(data['category_id'] == 24) - sum(data['category_id'] == 26)
+    indices = data[ (data['category_id'] == 24) ].index
+    randomIndices = np.random.choice(indices, sampleSize, replace = False)
+    data = data.drop(randomIndices, axis=0)
+    #undersample category 10
+    sampleSize = sum(data['category_id'] == 10) - sum(data['category_id'] == 26)
+    indices = data[ (data['category_id'] == 10) ].index
+    randomIndices = np.random.choice(indices, sampleSize, replace = False)
+    data = data.drop(randomIndices, axis=0)
     
-    #Perform k trials of model
-    sum = 0
-    k = 300
-    for i in range(k):
-        #split into training and testing sets
-        train,test = train_test_split(data, test_size = 0.2)
-        #create  model
-        X = train.drop(['category_id','video_id'], axis = 1)
-        #X = train[ ['meanSentiment','sentimentStdev','meanWordCount','wordCountStdev'] ]
-        Y = train['category_id']
-        #Init Model
-        clf = SVC()
-        #fit model
-        clf.fit(X,Y)
-        #summarize feature importance
-        # d = {}
-        # for feat, importance in zip(X.columns, clf.feature_importances_):
-        #     d[feat] = importance
-        # d = sorted(d.items(), key=operator.itemgetter(1))
-        # top10 = d[-10:]
-        # for i in range(10):
-        #     print(top10[i])
-        #predict on testing set
-        X = test.drop(['category_id','video_id'], axis = 1)
-        #X = test[ ['meanSentiment','sentimentStdev','meanWordCount','wordCountStdev'] ]
-        preds = clf.predict(X)
-        #get performance of model
-        Y = test['category_id']
-        score = accuracy_score(Y,preds)
-        sum += score
-    avg_score = sum/k
-    print(avg_score)    
+    
+    #split into training and testing sets
+    train,test = train_test_split(data, test_size = 0.2)
+    #create  model
+    X = train.drop(['category_id','video_id'], axis = 1)
+    #X = train[ ['meanSentiment','sentimentStdev','meanWordCount','wordCountStdev'] ]
+    Y = train['category_id']
+    #Init Model
+    clf = RandomForestClassifier()
+    #fit model
+    clf.fit(X,Y)
+    #summarize feature importance
+    # d = {}
+    # for feat, importance in zip(X.columns, clf.feature_importances_):
+    #     d[feat] = importance
+    # d = sorted(d.items(), key=operator.itemgetter(1))
+    # top10 = d[-10:]
+    # for i in range(10):
+    #     print(top10[i])
+    #predict on testing set
+    X = test.drop(['category_id','video_id'], axis = 1)
+    #X = test[ ['meanSentiment','sentimentStdev','meanWordCount','wordCountStdev'] ]
+    preds = clf.predict(X)
+    #get performance of model
+    Y = test['category_id']
+    score = accuracy_score(Y,preds)
+    print(score)    
